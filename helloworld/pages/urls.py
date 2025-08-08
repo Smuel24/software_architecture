65754668eb20f632efa7475b48e098fa091785b6
Samuel Molina <samumoe2401@gmail.com>
2025-08-07 20:21:15 -0500

    tutorial 2

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView
+from .views import AboutPageView
+from .views import ProductIndexView
+from .views import ProductShowView
+from .views import ProductCreateView
+from .views import ProductCreateView
 
 urlpatterns = [
-    path("", homePageView, name="home"),
+     path('', HomePageView.as_view(), name='home'),
+     path('about/', AboutPageView.as_view(), name='about'),
+     path('products/', ProductIndexView.as_view(), name ='index'),
+     path('products/<str:id>', ProductShowView.as_view(), name='show'),
+     path('products/create', ProductCreateView.as_view(), name = 'form'),
+     path('products/created/', ProductIndexView.as_view(), name='product-created'), 
 ]
--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -1,5 +1,96 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView
+from django.views import View
+from django import forms
+from django.urls import reverse
+from .models import Product
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseRedirect
+from .models import Product
 
-def homePageView(request):
-    return HttpResponse('Hello World!')
+
+
+class HomePageView(TemplateView):
+    template_name = "pages/home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Your Name",
+        })
+        return context
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {
+            "title": "Products - Online Store",
+            "subtitle": "List of products",
+            "products": Product.objects.all()
+        }
+        return render(request, self.template_name, viewData)
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+    def get(self, request, id):
+        try:
+            product_id = int(id)
+            if product_id < 1:
+                raise ValueError("Invalid ID")
+            product = get_object_or_404(Product, pk=product_id)
+        except:
+            return HttpResponseRedirect(reverse('home'))
+
+        viewData = {
+            "title": product.name + " - Online Store",
+            "subtitle": product.name + " - Product inforamation",
+            "product": product
+        }
+        return render(request, self.template_name, viewData)
+
+
+class ProductForm(forms.ModelForm):
+    name = forms.CharField(required=True)
+    price = forms.FloatField(required=True)
+
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price is not None and price <= 0:
+            raise ValidationError('Price must be greater than zero.')
+        return price
+
+
+class ProductCreateView(View):
+    template_name = 'products/create.html'
+
+    def get(self, request):
+        form = ProductForm()
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] = form
+        return render(request, self.template_name, viewData)
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product-created') 
+        else:
+            viewData = {}
+            viewData["title"] = "Create product"
+            viewData["form"] = form
+            return render(request, self.template_name, viewData)
+
